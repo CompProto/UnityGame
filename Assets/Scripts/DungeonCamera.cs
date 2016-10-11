8f9b00c112e4eaffb42b6bab33b48c63fcd56da6
Jon Tvermose Nielsen <jon.tvermose.nielsen@gmail.com>
2016-10-11 20:45:31 +0200

    First Commit
    First original commit, including procedural cave generation, textures
    and a moveable player.

---
+++ Assets/Scripts/DungeonCamera.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DungeonCamera : MonoBehaviour
+{
+    public GameObject target;
+    public float damping = 1;
+    public float minZoom = 3;
+    public float maxZoom = 10;
+    Vector3 offset;
+
+    void Start()
+    {
+        offset = transform.position - target.transform.position;
+    }
+
+    void Update()
+    {
+        // Debug.Log(offset);
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        if (scroll != 0)
+        {
+            if(offset.magnitude < minZoom && scroll > 0)
+            {
+                return;
+            }
+            else if(offset.magnitude > maxZoom && scroll < 0)
+            {
+                return;
+            }
+            offset *= 1 - scroll;
+            // Debug.Log(Input.GetAxis("Mouse ScrollWheel")); 
+        }
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 desiredPosition = target.transform.position + offset;
+        Vector3 position = Vector3.Lerp(transform.position, desiredPosition, Time.deltaTime * damping);
+        transform.position = position;
+
+        transform.LookAt(target.transform.position);
+    }
+}
---
+++ Assets/Scripts/ObjectSpawner.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectSpawner : MonoBehaviour {
+ 
+    public GameObject Player;
+    public GameObject Enemy;
+    public int EnemiesNumber = 10;
+
+    private MapGenerator mapGen;
+    private int width, height;
+
+	// Use this for initialization
+	void Start () {
+        mapGen = GetComponent<MapGenerator>();
+        mapGen.SetPlayer(Player);
+        mapGen.SetEnemy(Enemy);
+        mapGen.SetEnemiesNumber(EnemiesNumber);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+}
