c4c83873829e8bc5a3e2edd6e4f3cd8f17e700d0
Jon Tvermose Nielsen <jon.tvermose.nielsen@gmail.com>
2016-10-15 11:08:25 +0200

    Level up effect 3
    Using a particle attractor for the dust

---
+++ Assets/Scripts/ParticleManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using Particle = UnityEngine.ParticleSystem.Particle;
+
+public class ParticleManager : MonoBehaviour {
+
+    public GameObject target;
+    private ParticleSystem p;
+
+	// Use this for initialization
+	void Start () {
+        p = GetComponent<ParticleSystem>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        int count = p.particleCount;
+        ParticleSystem.Particle[] particles = new ParticleSystem.Particle[count];
+        p.GetParticles(particles);
+
+	    for(int i=0; i < count; i++)
+        {
+            particles[i].position = Vector3.Lerp(particles[i].position, target.transform.position,Time.deltaTime / 2.0f);
+        }
+
+        p.SetParticles(particles, count);
+	}
+}
