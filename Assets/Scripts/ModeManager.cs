2b2f39628f02cbf95ff25765d192a1e7694d9c36
Jon Tvermose Nielsen <jon.tvermose.nielsen@gmail.com>
2016-10-29 19:53:30 +0200

    Dark/White mode tweak
    Now lerping between materials for a smooth effect

--- Assets/Scripts/ModeManager.cs
+++ Assets/Scripts/ModeManager.cs
@@ -1,19 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class ModeManager : MonoBehaviour {
+public class ModeManager : MonoBehaviour
+{
 
     public AudioClip DayMusic, ShadowMusic;
     public GameObject[] gameObjects;
-    public Material[] materials;
+    public Material[] materials; // There must be 2 materials for each gameobject. First material is the white version, second material is dark version
 
-    public bool isDarkMode = false;
+    public bool isDarkMode = false; 
+
+    public float duration = 2.0f; // How long to swap between the materials
 
     private AudioSource source;
+    private float timer = 1.0f;
 
-	// Use this for initialization
-	void Start () {
-	    if(gameObjects.Length * 2 != materials.Length)
+    // Use this for initialization
+    void Start()
+    {
+        if (gameObjects.Length * 2 != materials.Length)
         {
             Debug.Log("ERROR! There should be exactly 2 materials pr. gameobject in the ModeManager");
         }
@@ -35,12 +40,10 @@ public class ModeManager : MonoBehaviour {
     {
         if (source.clip == sound && source.isPlaying)
             return;
-
         if (source.isPlaying)
         {
             source.Stop();
         }
-
         source.clip = sound;
         source.Play();
     }
@@ -49,19 +52,26 @@ public class ModeManager : MonoBehaviour {
     {
         isDarkMode = !isDarkMode;
         Debug.Log("Changing materials and music.");
-        int offset = 0;
         if (isDarkMode)
-        {
-            offset = 1;
             PlayShadowMusic();
-        }
         else
-        {
             PlayDayMusic();
-        }
-        for(int i=0; i < gameObjects.Length; i++)
+        timer = -duration;
+    }
+
+    void Update()
+    {
+        if (timer < 0.0f)
         {
-            gameObjects[i].GetComponent<Renderer>().material = materials[i * 2 + offset];
+            float lerp = Mathf.PingPong(Time.time, 2.0f) / 2.0f;
+            for (int i = 0; i < gameObjects.Length; i++)
+            {
+                if (isDarkMode)
+                    gameObjects[i].GetComponent<Renderer>().material.Lerp(materials[2 * i], materials[2 * i + 1], timer + 2);
+                else
+                    gameObjects[i].GetComponent<Renderer>().material.Lerp(materials[2 * i + 1], materials[2 * i], timer + 2);
+            }
         }
+        timer += Time.deltaTime;
     }
 }
